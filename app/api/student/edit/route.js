0456e4e3b002a48a17c168ec5ef70a59666559ce
NoarMiller <johnnydpx0@gmail.com>
2024-11-16 00:06:06 +0700

    no cache

--- app/api/student/edit/route.js
+++ app/api/student/edit/route.js
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server"
 import connectDB from "@/lib/mongodb"
 import Student from "@/models/student"
-import { revalidatePath } from "next/cache"
 
 export async function PUT(req) {
     const { studentId, firstNameEng, lastNameEng, firstNameLao, lastNameLao, gen, gender, birthday } = await req.json()
@@ -11,9 +10,6 @@ export async function PUT(req) {
             return NextResponse.json({ message: 'Student ID is required' }, { status: 400 })
         }
         await Student.findOneAndUpdate({ studentId }, { firstNameEng, lastNameEng, firstNameLao, lastNameLao, gen, gender, birthday })
-        revalidatePath("/");
-        revalidatePath("/admin/dashboard/studentscore");
-        revalidatePath("/admin/dashboard/editstudent");
         return NextResponse.json({ message: 'Student updated successfully' }, { status: 200 })
     } catch (error) {
         return NextResponse.json({ message: 'Error updating student' }, { status: 500 })
