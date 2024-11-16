73e69d53322b15156df3f5e9b0a58c2d24759273
NoarMiller <johnnydpx0@gmail.com>
2024-11-16 11:25:12 +0700

    cache

--- app/api/student/route.js
+++ app/api/student/route.js
@@ -1,13 +1,13 @@
 import connectDB from "../../../lib/mongodb";
 import Student from "../../../models/student";
 import { NextResponse } from "next/server";
-// import { revalidatePath } from "next/cache";
+import { revalidatePath } from "next/cache";
 
 export async function GET(req) {
     try {
         await connectDB();
         const students = await Student.find();
-        // revalidatePath("/")
+        revalidatePath("/")
         return NextResponse.json(students);
     } catch (error) {
         return NextResponse.json({ message: error.message }, { status: 500 });
